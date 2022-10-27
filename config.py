c050ed739997d7d9029d8c748ff78adff1de7427
Priyansh-J-Shah <spriyansh78@gmail.com>
2022-10-27 13:54:41 +0530

    Project Configuration

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+Pune_CSV_FILE_PATH = os.path.join("Model","Pune_House_Data.csv")
+
+Pune_MODEL_FILE_PATH = os.path.join("Model","Linear_Regression_Pune_House_Data.pkl")
+
+Pune_JSON_FILE_PATH = os.path.join("Model","Linear_Regression_Pune_House_Data.json")
+
+PORT_NUMBER = 5300
